0abff64617d4055398b02a5f691238545c4747c2
Jadiel Santana <jadiel.bsantana@gmail.com>
2022-02-27 23:15:49 -0300

    Ensure RemoteAuthentication throws UnexpectedError on 400

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,6 +2,7 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:clean_flutter_app/domain/helpers/helpers.dart';
 import 'package:clean_flutter_app/domain/usecases/usecases.dart';
 
 import 'package:clean_flutter_app/data/http/http.dart';
@@ -14,14 +15,15 @@ void main() {
   String url;
   RemoteAuthentication sut;
 
-  setUp((){
+  setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
   test('Should call HttpClient with correct values', () async {
-    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -29,5 +31,21 @@ void main() {
       method: 'post',
       body: {'email': params.email, 'password': params.secret},
     ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () {
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+    final future = sut.auth(params);
+    
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
