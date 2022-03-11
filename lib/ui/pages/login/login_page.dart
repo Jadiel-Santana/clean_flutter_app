a4a79b3a902621a5cac987f7224b80d21d94089d
Jadiel Santana <jadiel.bsantana@gmail.com>
2022-03-11 00:15:49 -0300

    ensure LoginPage presents no error if password is valid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -59,7 +59,9 @@ class LoginPage extends StatelessWidget {
                                 Icons.lock,
                                 color: Theme.of(context).primaryColorLight,
                               ),
-                              errorText: snapshot.data,
+                              errorText: snapshot.data?.isEmpty == true
+                                ? null
+                                : snapshot.data,
                             ),
                             obscureText: true,
                           );
