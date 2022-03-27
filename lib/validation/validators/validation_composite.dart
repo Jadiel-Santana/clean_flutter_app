0665b0a187bc285e67ccaba6a74a40edd8b5235f
Jadiel Santana <jadiel.bsantana@gmail.com>
2022-03-27 16:01:54 -0300

    Move ValidationComposite to its own file

---
+++ lib/validation/validators/validation_composite.dart
@@ -0,0 +1,24 @@
+import 'package:meta/meta.dart';
+
+import '../../presentation/protocols/protocols.dart';
+
+import '../protocols/protocols.dart';
+
+class ValidationComposite implements Validation {
+  final List<FieldValidation> validations;
+
+  ValidationComposite({@required this.validations});
+
+  @override
+  String validate({@required String field, @required String value}) {
+    String error;
+
+    for (final validation in validations.where((v) => v.field == field)) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    return error;
+  }
+}
