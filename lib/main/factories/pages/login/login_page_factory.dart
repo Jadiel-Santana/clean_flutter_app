84a824702d50dde44ca8e0d7308614e834218afb
Jadiel Santana <jadiel.bsantana@gmail.com>
2022-03-28 16:57:26 -0300

    Add GetxLoginPresenter

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -5,5 +5,6 @@ import '../../factories.dart';
 import '../../../../ui/pages/pages.dart';
 
 Widget makeLoginPage() => LoginPage(
-  presenter: makeLoginPresenter(),
+  //presenter: makeStreamLoginPresenter(),
+  presenter: makeGetxLoginPresenter(),
 );
--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -4,8 +4,15 @@ import '../../../../presentation/presenters/presenters.dart';
 
 import '../../../../ui/pages/pages.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+    validation: makeLoginValidation(),
+    authentication: makeRemoteAuthentication(),
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
     validation: makeLoginValidation(),
     authentication: makeRemoteAuthentication(),
   );
